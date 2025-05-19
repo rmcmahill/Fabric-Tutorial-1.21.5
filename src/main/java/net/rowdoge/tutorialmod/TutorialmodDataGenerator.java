8f1e7e5af8de950d9b5df31de92f79f02e669401
rmcmahill <rowanmcmahill@gmail.com>
2025-05-18 20:39:19 -0400

    Initial commit

---
+++ src/main/java/net/rowdoge/tutorialmod/TutorialmodDataGenerator.java
@@ -0,0 +1,11 @@
+package net.rowdoge.tutorialmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TutorialmodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
